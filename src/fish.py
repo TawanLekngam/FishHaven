b578c39be528e9f6bb447a14a3efa470739523b6
Tawan Lekngam <tawancraft@outlook.com>
2023-02-06 15:26:14 +0700

    [Update] LIFETIME AND DASHBOARD FEATURE

--- src/fish.py
+++ src/fish.py
@@ -1,52 +1,73 @@
+import os
 import random
 import pygame
 
+from data.fishData import FishData
+
+
+BASE_DIR = os.path.split(os.path.abspath(__file__))[0]
+ASSET_DIR = os.path.join(BASE_DIR, "assets")
+
 
 class Fish(pygame.sprite.Sprite):
-    def __init__(self, genesis: str):
+    def __init__(self, genesis: str, data: FishData = None):
         super().__init__()
+        self.data = data if data else FishData(genesis)
 
+        # pygame objects
         self.direction = random.choice(["left", "right"])
-        self.current_frame = 0
+        self.frame = 0
         self.sprites: dict[str, list[pygame.Surface]] = {
             "left": [],
             "right": []
         }
-
         self.__load_sprite(genesis)
-        self.image = self.sprites[self.direction][self.current_frame]
+        self.image = self.sprites[self.direction][self.frame]
         self.rect = self.image.get_rect()
         self.rect.x = random.randint(0, 1280 - self.rect.width)
         self.rect.y = random.randint(0, 720 - self.rect.height)
+        self.speed = random.randint(1, 3)
 
     def __load_sprite(self, genesis: str):
         pond_type = "local_pond" if genesis == "doo-pond" else "foreign_pond"
-        base_path = f"./src/assets/{pond_type}"
+        target_path = os.path.join(ASSET_DIR, pond_type)
 
         for i in range(1, 5):
-            image_path = f"{base_path}/{i}.png"
+            image_path = f"{target_path}/{i}.png"
             image = pygame.image.load(image_path)
             image_left = pygame.transform.scale(image, (100, 100))
             image_right = pygame.transform.flip(image_left, True, False)
             self.sprites["left"].append(image_left)
             self.sprites["right"].append(image_right)
 
-        self.current_frame = 0
+        self.frame = 0
 
-    def move(self):
+    def __move(self):
         if self.direction == "left":
-            self.rect.x -= random.randint(1, 3)
+            self.rect.x -= self.speed
             if self.rect.x <= 0:
                 self.direction = "right"
         else:
-            self.rect.x += random.randint(1, 3)
+            self.rect.x += self.speed
             if self.rect.x >= 1280 - self.rect.width:
                 self.direction = "left"
 
+    def tick_lifetime(self):
+        if self.data.is_immortal:
+            return
+
+        if self.data.is_alive:
+            self.data.lifetime -= 1
+            self.data.time_in_pond += 1
+            if self.data.lifetime <= 0:
+                self.data.is_alive = False
+                self.kill()
+        print(f"id:{self.data.id} lefetime:{self.data.lifetime}")
+
     def update(self):
-        self.current_frame = (self.current_frame + 0.1) % len(self.sprites[self.direction])
-        self.image = self.sprites[self.direction][int(self.current_frame)]
-        self.move()
+        self.frame = (self.frame + 0.1) % len(self.sprites[self.direction])
+        self.image = self.sprites[self.direction][int(self.frame)]
+        self.__move()
 
     def die(self):
         self.kill()
--- src/pond.py
+++ src/pond.py
@@ -1,7 +1,12 @@
 import random
 import pygame
+import sys
+import threading
+
+from PySide6.QtWidgets import QApplication
 
 from data.pondData import PondData
+from components import dashboard
 from fish import Fish
 
 
@@ -10,14 +15,15 @@ class Pond:
 
     def __init__(self):
         self.name: str = "doo-pond"
-        self.all_sprites = pygame.sprite.Group()
+        self.all_sprites: pygame.sprite.Group[Fish] = pygame.sprite.Group()
 
     def spawn_fish(self, genesis):
         fish = Fish(genesis)
         self.all_sprites.add(fish)
 
-    def migrate(self):
-        pass
+    def __tick_lifetime(self):
+        for fish in self.all_sprites:
+            fish.tick_lifetime()
 
     def run(self):
         pygame.init()
@@ -27,8 +33,11 @@ class Pond:
         pygame.display.set_caption("Fish Haven [Doo Pond]")
         clock = pygame.time.Clock()
 
+        update_time = pygame.time.get_ticks()
+
         self.spawn_fish("doo-pond")
-        self.spawn_fish("other")
+
+        app = QApplication(sys.argv)
 
         running = True
         while running:
@@ -36,6 +45,21 @@ class Pond:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     running = False
+
+                # handle press key event
+                if event.type == pygame.KEYDOWN:
+                    print("press button")
+                    if event.key == pygame.K_LEFT:
+                        print("open dashboard")
+                        d = dashboard.DashBoard()
+                        pond_handler = threading.Thread(target=app.exec_)
+                        pond_handler.start()
+                    
+
+            time_since_update = pygame.time.get_ticks() - update_time
+            if (time_since_update >= 1000):
+                self.__tick_lifetime()
+                update_time = pygame.time.get_ticks()
 
             self.all_sprites.update()  # update all sprites in the group
             screen.blit(bg, (0, 0))  # render background image
