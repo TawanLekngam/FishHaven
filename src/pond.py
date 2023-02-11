1e48bec899be7a2b4ff219342d9207048a11bc79
Dee Deesit <40157947+deeckn@users.noreply.github.com>
2023-02-11 15:19:49 +0700

    Refactor

--- src/pond.py
+++ src/pond.py
@@ -1,13 +1,12 @@
-import random
 import pygame
 import sys
 import threading
 
 from PySide6.QtWidgets import QApplication
 
-from data.pondData import PondData
-from components import dashboard
-from fish import Fish
+from components import Dashboard
+from FishSprite import FishSprite
+from utils.FishFactory import FishFactory
 
 
 class Pond:
@@ -15,62 +14,60 @@ class Pond:
 
     def __init__(self):
         self.name: str = "doo-pond"
-        self.all_sprites: pygame.sprite.Group[Fish] = pygame.sprite.Group()
+        self.all_sprites: pygame.sprite.Group[FishSprite] = pygame.sprite.Group(
+        )
 
-    def spawn_fish(self, genesis):
-        fish = Fish(genesis)
-        self.all_sprites.add(fish)
+    def spawn_fish(self):
+        fish = FishFactory.generate_fish()
+        fish_sprite = FishSprite(fish)
+        self.all_sprites.add(fish_sprite)
 
-    def __tick_lifetime(self):
+    def __tick_lifespan(self):
         for fish in self.all_sprites:
-            fish.tick_lifetime()
+            fish.tick_lifespan()
+
+    def load_fishes(self):
+        # TODO: Replace with database fetches
+        for _ in range(5):
+            self.spawn_fish()
 
     def run(self):
         pygame.init()
         screen = pygame.display.set_mode(Pond.__WINDOW_SIZE)
-        bg = pygame.image.load("./src/assets/background.jpg")
-        bg = pygame.transform.scale(bg, Pond.__WINDOW_SIZE)
+        background = pygame.image.load("./src/assets/background.jpg")
+        background = pygame.transform.scale(background, Pond.__WINDOW_SIZE)
         pygame.display.set_caption("Fish Haven [Doo Pond]")
         clock = pygame.time.Clock()
-
         update_time = pygame.time.get_ticks()
 
-        self.spawn_fish("doo-pond")
+        self.load_fishes()
 
         app = QApplication(sys.argv)
 
         running = True
         while running:
-
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     running = False
 
-                # handle press key event
                 if event.type == pygame.KEYDOWN:
                     print("press button")
                     if event.key == pygame.K_LEFT:
                         print("open dashboard")
-                        d = dashboard.DashBoard()
+                        _ = Dashboard.DashBoard()
                         pond_handler = threading.Thread(target=app.exec_)
                         pond_handler.start()
-                    
 
             time_since_update = pygame.time.get_ticks() - update_time
-            if (time_since_update >= 1000):
-                self.__tick_lifetime()
+            if time_since_update >= 1000:
+                self.__tick_lifespan()
                 update_time = pygame.time.get_ticks()
 
             self.all_sprites.update()  # update all sprites in the group
-            screen.blit(bg, (0, 0))  # render background image
+            screen.blit(background, (0, 0))  # render background image
             self.all_sprites.draw(screen)  # draw all sprites in the group
 
             pygame.display.flip()
             clock.tick(60)
 
         pygame.quit()
-
-
-if __name__ == "__main__":
-    p = Pond()
-    p.run()
---
+++ src/utils/FishFactory.py
@@ -0,0 +1,49 @@
+import math
+import random
+from models.Fish import Fish
+
+
+class FishFactory:
+    @staticmethod
+    def generate_fish() -> Fish:
+        "returns a Fish object with random properties"
+
+        fish_id = FishFactory.__rand_id()
+        genesis = 'doo-pond'
+        gender = random.choice(['male', 'female'])
+        pheromone_threshold = FishFactory.__rand_pheromone_threshold()
+        crowd_threshold = FishFactory.__rand_crowd_threshold()
+        lifespan = FishFactory.__rand_lifespan()
+
+        fish = Fish(
+            fish_id,
+            genesis,
+            gender,
+            pheromone_threshold,
+            crowd_threshold, lifespan
+        )
+        return fish
+
+    @staticmethod
+    def __rand_id() -> str:
+        "returns a random 6 digit string"
+        id = ""
+        for _ in range(6):
+            id += "0123456789"[math.floor(random.random() * 10)]
+        return id
+
+    @staticmethod
+    def __rand_pheromone_threshold() -> int:
+        return random.randint(30, 60)
+
+    @staticmethod
+    def __rand_crowd_threshold() -> int:
+        return random.randint(5, 20)
+
+    @staticmethod
+    def __rand_lifespan() -> int:
+        """
+        returns a random value between 60 and 120 with 99% probability,
+        and returns 0 with 1% probability.
+        """
+        return 0 if random.random() <= 0.01 else random.randint(60, 120)
