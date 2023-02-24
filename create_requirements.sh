6a7271ce9b862f497e5d440b2db8070cc9f8ca2c
Tawan Lekngam <tawancraft@outlook.com>
2023-02-24 14:36:27 +0700

    Adjust file structure

--- create_requirements.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-REQ_FILE=requirements.txt
-
-if [ -f $REQ_FILE ]; then
-    rm $REQ_FILE
-fi
-
-pip freeze > $REQ_FILE
--- install_requirements.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-REQ_FILE=requirements.txt
-
-if [ -f $REQ_FILE ]; then
-    pip install -r $REQ_FILE
-else
-    echo "Error: $REQ_FILE not found"
-fi
