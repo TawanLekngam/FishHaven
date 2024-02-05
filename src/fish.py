961aaffe8fec8c771b8aee9660b354ea73209361
TawanL <tawancraft@outlook.com>
2024-02-05 08:45:18 +0700

    Add vertical movement to Fish class

--- src/fish.py
+++ src/fish.py
@@ -20,6 +20,8 @@ SCALE = (100, 100)
 class Direction(Enum):
     LEFT = 0
     RIGHT = 1
+    UP = 2
+    DOWN = 3
 
 
 class Fish(Sprite):
@@ -32,8 +34,10 @@ class Fish(Sprite):
         self._loadSkin(model.genesis)
         self.image: Surface = Fish.skinL[model.genesis][0]
         self.frame = 0
-        self.direction: Direction = Direction.RIGHT
+        self.directionX: Direction = Direction.RIGHT
+        self.directionY: Direction = Direction.UP
         self.rect: Rect = self.image.get_rect()
+        self.speed = 3
 
     def _loadSkin(self, genesis: str):
         if genesis not in Fish.skinL:
@@ -41,33 +45,39 @@ class Fish(Sprite):
 
             if isdir(path):
                 files = [f for f in listdir(path) if f.endswith(".png")]
-                print("f", files)
-                print("g", genesis)
-                Fish.skinL[genesis] = [
-                    scale(load(join(path, f)), SCALE) for f in files]
+                Fish.skinL[genesis] = [scale(load(join(path, f)), SCALE)
+                                       for f in files]
                 Fish.skinR[genesis] = [flip(s, True, False)
                                        for s in Fish.skinL[genesis]]
 
     def update(self):
-        # self.movement.move(self)
         self.move()
 
     def updateImage(self):
-        if self.direction == Direction.LEFT:
+        if self.directionX == Direction.LEFT:
             self.image = Fish.skinL[self.model.genesis][int(self.frame)]
         else:
             self.image = Fish.skinR[self.model.genesis][int(self.frame)]
 
     def move(self):
         screenRect = get_surface().get_rect()
-        if self.direction == Direction.LEFT:
-            self.rect.x -= 1
+        if self.directionX == Direction.LEFT:
+            self.rect.x -= self.speed
             if self.rect.left < screenRect.left:
-                self.direction = Direction.RIGHT
-
+                self.directionX = Direction.RIGHT
         else:
-            self.rect.x += 1
+            self.rect.x += self.speed
             if self.rect.right > screenRect.right:
-                self.direction = Direction.LEFT
+                self.directionX = Direction.LEFT
+
+        if self.directionY == Direction.UP:
+            self.rect.y -= self.speed
+            if self.rect.top < screenRect.top:
+                self.directionY = Direction.DOWN
+        else:
+            self.rect.y += self.speed
+            if self.rect.bottom > screenRect.bottom:
+                self.directionY = Direction.UP
+
         self.frame = (self.frame + 0.1) % len(Fish.skinL[self.model.genesis])
         self.updateImage()
